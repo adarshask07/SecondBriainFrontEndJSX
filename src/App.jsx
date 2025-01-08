d97c7031a4a6963a71772fcc77cff780a70085a1
adarshask07 <119163391+adarshask07@users.noreply.github.com>
2025-01-08 16:03:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-route
 import { useSelector } from 'react-redux';
 
 import Auth from './pages/Auth';
-import Hello from './pages/hello';
+
 import { AnimatedCard } from './Components/ui/AnimatedCard';
 import Memories from './pages/Memories';
 import MemoryPage from './pages/MemoryPage';
